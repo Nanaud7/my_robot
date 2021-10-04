c98db415ae75034aceec8b17da6d2e85112ee5f7
Nanaud7 <nanaud7@outlook.fr>
2021-10-04 19:46:53 +0200

    config code

--- Informatique/robot-teensy/src/Strategy.cpp
+++ Informatique/robot-teensy/src/Strategy.cpp
@@ -7,9 +7,9 @@ Strategy blueStrategy[4] = {
     {
         .x = 0, .y = 0, .dir = FORWARD
     }, {
-        .x = 100, .y = 100, .dir = FORWARD
+        .x = 100, .y = 0, .dir = FORWARD
     }, {
-        .x = -100, .y = -100, .dir = FORWARD
+        .x = 200, .y = 100, .dir = FORWARD
     }, {
         .x = 0, .y = 0, .dir = FORWARD
     }
